2134c6b46a7ea94f59bb3b735adb2f5da5501c03
Philipp Gruber <pgruber@astaro.com>
2010-09-02 14:20:15 +0200

    external config file

--- monitor.zsh
+++ monitor.zsh
@@ -1,11 +1,8 @@
 #!/bin/zsh
 
-host='yoshi.mega2000.de'
-host='www.google.de'
-ports=(22 25 53 80 443 993)
+source 'monitor.cfg'
+
 dead=()
-timeout=1
-mailto='philipp.gruber@flupps.net'
 
 for port ($ports) {
 	if (`nc -w $timeout -z $host $port`) {
@@ -14,9 +11,6 @@ for port ($ports) {
 		dead+=($port)
 	}
 }
-
-# echo "[$dead]"
-# echo ${#dead[@]}
 
 if [[ 0 -ne ${#dead[@]} ]] {
 	cat mail.tmpl | sed "s/!!HOST!!/$host/g; s/!!DEAD!!/$dead/g; s/!!PORTS!!/$ports/g;" | mail -s 'Monitoring-Error' $mailto
