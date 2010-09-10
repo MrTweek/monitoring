83aeea2d4987680f0a69aac394f9c768c44b6fe3
Philipp Gruber <pgruber@astaro.com>
2010-09-10 10:37:23 +0200

    minor fix

--- monitor.zsh
+++ monitor.zsh
@@ -16,7 +16,7 @@ if [[ 0 -ne ${#dead[@]} ]] {
 	message=`cat mail.tmpl | sed "s/!!HOST!!/$host/g; s/!!DEAD!!/$dead/g; s/!!PORTS!!/$ports/g;"`
 	echo $message | mail -s "[Monitor] Problems on $host" $mailto
 	if [[ 1 -eq $sendsms ]] {
-		./sipgateAPI-sms.pl "$username" "$password" "$number" "$message"
+		./sipgateAPI-sms.pl "$username" "$password" "$number" "'$message'"
 	}
 }
 
